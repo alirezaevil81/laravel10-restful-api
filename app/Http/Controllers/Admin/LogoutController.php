f36cc1a34b6349a9c1120d4d05d21acb66ec076d
alirezaharaji <alirezask385@gmail.com>
2025-11-22 17:06:46 +0330

    Add LogoutController and implement logout functionality

---
+++ app/Http/Controllers/Admin/LogoutController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use App\RestfulApi\Facades\ApiResponse;
+use Illuminate\Http\Request;
+
+class LogoutController extends Controller
+{
+    public function __invoke()
+    {
+        auth()->user()->currentAccessToken()->delete();
+
+        return ApiResponse::withMessage('Logged out successfully')->build()->response();
+    }
+}
--- routes/admin.php
+++ routes/admin.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Admin\ArticleController;
 use App\Http\Controllers\Admin\GetCurrentUserController;
 use App\Http\Controllers\Admin\LoginController;
+use App\Http\Controllers\Admin\LogoutController;
 use App\Http\Controllers\Admin\UserController;
 use Illuminate\Support\Facades\Route;
 
@@ -11,7 +12,10 @@ Route::post('/login', LoginController::class);
 
 
 Route::middleware('auth:sanctum')->group(function () {
+
     Route::get('current-user', GetCurrentUserController::class);
+    Route::delete('/logout', LogoutController::class);
+
     Route::apiResource('users', UserController::class);
     Route::apiResource('articles', ArticleController::class)->only(['index']);
 });
