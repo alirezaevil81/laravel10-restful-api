c5cb73598980dfb083ff98af5473dcb79bfb5bef
alirezaharaji <alirezask385@gmail.com>
2025-11-27 15:33:37 +0330

    Add login validation tests for admin panel authentication

--- tests/Feature/LoginToAdminPanelTest.php
+++ tests/Feature/LoginToAdminPanelTest.php
@@ -15,16 +15,37 @@ class LoginToAdminPanelTest extends TestCase
     /**
      * A basic feature test example.
      */
-    public function test_an_admin_can_login_to_admin_panel(): void
+
+
+    public function test_login_validation(): void
+    {
+        $response = $this->post('/api/admin/login');
+        $response->assertStatus(422);
+    }
+
+    public function test_an_admin_can_login_to_admin_panel_with_correct_data(): void
     {
         $response = $this->post('/api/admin/login', [
             'email' => 'admin@gmail.com',
             'password' => '12345678'
-        ])->assertStatus(200);
+        ]);
 
         $response->assertStatus(200);
         $response->assertJsonStructure(['token']);
 
 
     }
+
+    public function test_an_admin_cannot_login_to_admin_panel_with_wrong_data(): void
+    {
+        $response = $this->post('/api/admin/login', [
+            'email' => 'admin@gmail.com',
+            'password' => '123'
+        ]);
+
+        $response->assertStatus(401);
+
+    }
+
+
 }
